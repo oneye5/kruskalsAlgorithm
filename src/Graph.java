2c2596cde9268df43a5784a6df7a328b7f92ef1d
oneye5 <ocjlaz@gmail.com>
2024-05-09 13:44:27 +1200

    update uh

--- src/Graph.java
+++ src/Graph.java
@@ -15,15 +15,15 @@ public class Graph
         vertexList.add(new Vertex("E"));
         vertexList.add(new Vertex("F"));
 
-        edgeList.add(new Edge(vertexList.get(0), vertexList.get(1), 2.0f));
-        edgeList.add(new Edge(vertexList.get(0), vertexList.get(2), 4.0f));
-        edgeList.add(new Edge(vertexList.get(1), vertexList.get(2), 1.0f));
-        edgeList.add(new Edge(vertexList.get(1), vertexList.get(3), 5.0f));
-        edgeList.add(new Edge(vertexList.get(1), vertexList.get(4), 3.0f));
-        edgeList.add(new Edge(vertexList.get(2), vertexList.get(3), 2.0f));
-        edgeList.add(new Edge(vertexList.get(3), vertexList.get(4), 6.0f));
-        edgeList.add(new Edge(vertexList.get(3), vertexList.get(5), 4.0f));
-        edgeList.add(new Edge(vertexList.get(4), vertexList.get(5), 1.0f));
+        edgeList.add(new Edge(vertexList.get(0), vertexList.get(1), 2.0f)); // (A)-(B)
+        edgeList.add(new Edge(vertexList.get(0), vertexList.get(2), 4.0f)); // (A)-(C)
+        edgeList.add(new Edge(vertexList.get(1), vertexList.get(2), 1.0f)); // (B)-(C)
+        edgeList.add(new Edge(vertexList.get(1), vertexList.get(3), 5.0f)); // (B)-(D)
+        edgeList.add(new Edge(vertexList.get(1), vertexList.get(4), 3.0f)); // (B)-(E)
+        edgeList.add(new Edge(vertexList.get(2), vertexList.get(3), 2.0f)); // (C)-(D)
+        edgeList.add(new Edge(vertexList.get(3), vertexList.get(4), 6.0f)); // (D)-(E)
+        edgeList.add(new Edge(vertexList.get(3), vertexList.get(5), 4.0f)); // (D)-(F)
+        edgeList.add(new Edge(vertexList.get(4), vertexList.get(5), 1.0f)); // (E)-(F)
 
 
         var result = new Kruskals().evaluate(this);
